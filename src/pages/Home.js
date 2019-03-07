6d235da8feebf3abced3d4cd9d9a544a1bb1bbda
Mario Rizki <mariorizki07@gmail.com>
2019-03-07 22:59:39 +0700

    implement jumbotron

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navigation from '../components/Navigation';
+import Jumbotron from '../components/Jumbotron';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Navigation />
+        <Jumbotron />
       </div>
     );
   }
