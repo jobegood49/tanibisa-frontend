e515d5a5f5ab7221bad8cdf32e777acdff86ac27
Jonathan Nicolas <jonathan.s.nicolas@gmail.com>
2019-03-07 17:08:56 +0700

    Create basic pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Commodities from './pages/Commodities'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <Switch>
+          <Route exact path={`/`} component={Home} />
+          <Route path={`/about`} component={About} />
+          <Route path={`/register`} component={Register} />
+          <Route path={`/login`} component={Login} />
+          <Route path={`/commodities`} component={Commodities} />
+        </Switch>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
