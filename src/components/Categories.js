cac455b23fe346da8bcee534b0f0edd32d365af4
Mario Rizki <mariorizki07@gmail.com>
2019-03-08 19:12:46 +0700

    implement categories component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -5,9 +5,11 @@ import styled from 'styled-components';
 const StyledSection = styled.section`
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const StyledDiv = styled.div`
+const TitleDiv = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 100px;
@@ -18,7 +20,7 @@ const Title = styled.div`
   justify-content: center;
 `;
 
-const StyledImg = styled.img`
+const TitleIcon = styled.img`
   height: 64px;
   width: 64px;
   margin-right: 30px;
@@ -35,16 +37,61 @@ const StyledHr = styled.hr`
   border-top: 3px solid #6dcc84;
 `;
 
+const IconContainers = styled.div`
+  display: flex;
+  justify-content: space-around;
+  box-shadow: 0 3px 20px rgba(0, 0, 0, 0.5);
+  width: 1200px;
+  margin: 30px 0;
+  border-radius: 10px;
+`;
+
+const IconContainer = styled.span`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 0;
+`;
+
+const CommoditiesIcon = styled.img`
+  height: 146px;
+`;
+
+const CommoditiesText = styled.h3`
+  font-size: 24px;
+`;
+
 const Categories = () => {
   return (
     <StyledSection>
-      <StyledDiv>
+      <TitleDiv>
         <Title>
-          <StyledImg src="/assets/images/categories-icon.svg" alt="" />
+          <TitleIcon
+            src="/assets/images/categories-icon.svg"
+            alt="Commodites"
+          />
           <StyledH1>Product Categories</StyledH1>
         </Title>
         <StyledHr />
-      </StyledDiv>
+      </TitleDiv>
+      <IconContainers>
+        <IconContainer>
+          <CommoditiesIcon src="/assets/images/fruits-icon.png" alt="Fruits" />
+          <CommoditiesText>Fruits</CommoditiesText>
+        </IconContainer>
+        <IconContainer>
+          <CommoditiesIcon
+            src="/assets/images/vegetables-icon.png"
+            alt="Vegetables"
+          />
+          <CommoditiesText>Vegetables</CommoditiesText>
+        </IconContainer>
+        <IconContainer>
+          <CommoditiesIcon src="/assets/images/rice-icon.png" alt="Rice" />
+          <CommoditiesText>Rice</CommoditiesText>
+        </IconContainer>
+      </IconContainers>
     </StyledSection>
   );
 };
