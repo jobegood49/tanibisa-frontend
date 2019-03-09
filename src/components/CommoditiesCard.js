3854bef27010483c60e9d7da83b28519ca0eba77
Mario Rizki <mariorizki07@gmail.com>
2019-03-09 18:49:44 +0700

    implement Commodities Cards & Commodities Card components

---
+++ src/components/CommoditiesCard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.9);
+  margin: 10px 10px;
+`;
+
+const CommoditiesCard = ({ item }) => {
+  return (
+    <Card>
+      <img src={item.image} alt={item.name} />
+      <h3>{item.name}</h3>
+      <hr />
+      <p>{item.price}</p>
+    </Card>
+  );
+};
+
+export default CommoditiesCard;
--- src/components/RecommendedCommodities.js
+++ src/components/RecommendedCommodities.js
@@ -1,79 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-class RecommendedCommodities extends Component {
-  constructor() {
-    super();
-    this.state = {
-      commodities: [
-        {
-          id: 1,
-          title: 'Red Apple',
-          description: 'The description of the red apple',
-          image: 'image of the red apple'
-        },
-        {
-          id: 2,
-          title: 'Green Mongo',
-          description: 'Description of the green mongo',
-          image: 'image of the green mongo'
-        },
-        {
-          id: 3,
-          title: 'Yellow Banana',
-          description: 'Description of the yellow banana',
-          image: 'image of the yellow banana'
-        }
-      ]
-    };
-  }
-  render() {
-    const Section = styled.section`
-      display: flex;
-      justify-content: center;
-      flex-direction: column;
-      align-items: center;
-    `;
+import CommoditiesCards from '../components/CommoditiesCards';
 
-    const TitleDiv = styled.div`
-      display: flex;
-      flex-direction: column;
-      margin-top: 100px;
-    `;
+const Section = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
 
-    const Title = styled.div`
-      display: flex;
-      justify-content: center;
-    `;
+const TitleDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 100px;
+`;
 
-    const TitleIcon = styled.img`
-      height: 64px;
-      width: 64px;
-      margin-right: 30px;
-    `;
+const Title = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
-    const StyledH1 = styled.h1`
-      font-size: 30px;
-    `;
+const TitleIcon = styled.img`
+  height: 64px;
+  width: 64px;
+  margin-right: 30px;
+`;
 
-    const StyledHr = styled.hr`
-      margin-top: -15px;
-      width: 499px;
-      border-top: 3px solid #6dcc84;
-    `;
+const StyledH1 = styled.h1`
+  font-size: 30px;
+`;
 
-    return (
-      <Section>
-        <TitleDiv>
-          <Title>
-            <TitleIcon src="/assets/images/recommended-icon.svg" alt="" />
-            <StyledH1>Recommended Commodities</StyledH1>
-          </Title>
-          <StyledHr />
-        </TitleDiv>
-      </Section>
-    );
-  }
-}
+const StyledHr = styled.hr`
+  margin-top: -15px;
+  width: 499px;
+  border-top: 3px solid #6dcc84;
+`;
+
+const RecommendedCommodities = () => {
+  return (
+    <Section>
+      <TitleDiv>
+        <Title>
+          <TitleIcon src="/assets/images/recommended-icon.svg" alt="" />
+          <StyledH1>Recommended Commodities</StyledH1>
+        </Title>
+        <StyledHr />
+      </TitleDiv>
+      <CommoditiesCards />
+    </Section>
+  );
+};
 
 export default RecommendedCommodities;
