d5f749be4e0b10ae004de3f4153af3b200118153
Mario Rizki <mariorizki07@gmail.com>
2019-03-09 18:53:54 +0700

    add little styling to card

--- src/components/CommoditiesCard.js
+++ src/components/CommoditiesCard.js
@@ -4,6 +4,10 @@ import styled from 'styled-components';
 const Card = styled.div`
   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.9);
   margin: 10px 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const CommoditiesCard = ({ item }) => {
