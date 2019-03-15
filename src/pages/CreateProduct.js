d951325e36addd13368ff126cd35a7da8020d3a8
Jonathan Nicolas <jonathan.s.nicolas@gmail.com>
2019-03-15 17:55:32 +0700

    Add create product page

---
+++ src/pages/CreateProduct.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Navigation from '../components/Navigation'
+
+import Footer from '../components/Footer'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <p>THIS IS THE CREATE PRODUCT PAGE</p>
+
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default Home
--- src/pages/ProfileBuyer.js
+++ src/pages/ProfileBuyer.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Navigation from '../components/Navigation';
@@ -10,6 +11,14 @@ const StyledDiv = styled.div`
   justify-content: center;
   margin: 100px 0;
 `;
+=======
+import React, { Component } from 'react'
+import Navigation from '../components/Navigation'
+import { connect } from 'react-redux'
+import { getOneBuyer } from '../redux/actions/buyers'
+import Footer from '../components/Footer'
+import { push } from 'connected-react-router'
+>>>>>>> Add create product page
 
 class ProfileFarmer extends Component {
   componentDidMount() {
@@ -17,10 +26,15 @@ class ProfileFarmer extends Component {
 
     this.props.dispatch(getOneBuyer(token));
   }
+
+  createProduct = () => {
+    this.props.dispatch(push('/createProduct'))
+  }
   render() {
     return (
       <div>
         <Navigation />
+<<<<<<< HEAD
         <StyledDiv>
           <div>
             <img src="/assets/images/avatar.svg" alt="" />
@@ -31,6 +45,11 @@ class ProfileFarmer extends Component {
           </div>
         </StyledDiv>
 
+=======
+        <h1>{this.props.buyers && this.props.buyers.email}</h1>
+        <h1>{this.props.buyers && this.props.buyers.name}</h1>
+        <button onClick={this.createProduct}>Create A Product</button>
+>>>>>>> Add create product page
         <Footer />
       </div>
     );
